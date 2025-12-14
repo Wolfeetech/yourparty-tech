c79f129370b8c4331194f2c1e082a90ecef62747
wwolfitec <wwolfitec@gmail.com>
2025-12-14 22:21:43 +0100

    feat: Implement core theme functions for asset loading, customizer settings, routing, and security.

--- yourparty-tech/functions.php
+++ yourparty-tech/functions.php
@@ -258,6 +258,8 @@ add_action('init', function () {
         flush_rewrite_rules();
         update_option('yourparty_rules_flushed_v3', true);
     }
+    
+    header('X-Debug-Deploy: v_final_check_1');
 });
 
 add_filter('query_vars', function ($vars) {
